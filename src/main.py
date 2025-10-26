386648cefe30026a2e5bed56e0fb9edafb4dedc4
Sivanesan <112844817+Sivanesan-nst@users.noreply.github.com>
2025-10-26 10:05:33 +0530

    feat(config): implement environment-based application startup with basic settings

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
 from fastapi import FastAPI
+from src.core.config import get_settings
+import sys
 
+def get_env_from_args():
+    if "--env" in sys.argv:
+        index = sys.argv.index("--env")
+        if index + 1 < len(sys.argv):
+            return sys.argv[index + 1]
+    return None
+
+env = get_env_from_args()
+settings = get_settings(env)
+print(settings)
 app = FastAPI(title="Smart CRM", 
               description=" AI Enabled CRM",
               version="1.0.0")
